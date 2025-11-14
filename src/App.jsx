e312a30873bc7133f526dd223dba39fef391ec3b
BanLowi <lowi40k@gmail.com>
2025-11-14 12:16:39 +0100

    header component and layout initialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Header from "./components/Header"
+
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+
+    return (
+        <header>
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <NavLink to="/" className="navbar-brand"> Fake Store </NavLink>
+                    <div class="collapse navbar-collapse" id="navbarID">
+                        <div class="navbar-nav">
+                            <NavLink to="/" className="nav-link active">Home Page</NavLink>
+                            <NavLink to="/about" className="nav-link">Chi Siamo</NavLink>
+                            <NavLink to="/store" className="nav-link">Prodotti</NavLink>
+
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header >
+    )
+}
